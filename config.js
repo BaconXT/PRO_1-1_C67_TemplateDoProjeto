afd2772d81b17a74fde414db9074316f70f46d97
BaconXT <93720121+BaconXT@users.noreply.github.com>
2022-08-18 10:47:49 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,14 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
+const firebaseConfig = {
+  apiKey: "AIzaSyB8M1IICjbXjDTrPCO7snka8gMdRBhyw28",
+  authDomain: "equipedevotacao.firebaseapp.com",
+  projectId: "equipedevotacao",
+  storageBucket: "equipedevotacao.appspot.com",
+  messagingSenderId: "277407288562",
+  appId: "1:277407288562:web:84f204f578b66059e6546b"
+};
 
 };
 // Initialice o Firebase
